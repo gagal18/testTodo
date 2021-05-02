49440e5ca09b5fea2973664ec319db29e7cf7fb2
gagal18 <gagaleskibojan@gmail.com>
2021-05-02 16:30:23 +0200

    deleteBTN

--- src/component/ToDoList/ToDoList.js
+++ src/component/ToDoList/ToDoList.js
@@ -1,76 +1,80 @@
-import { Typography , TextField , Button , Avatar } from '@material-ui/core';
-import {useEffect ,useState} from 'react'
+import { Typography, TextField, Button, Avatar } from '@material-ui/core';
+import { useEffect, useState } from 'react'
 import Aux from '../../hoc/Auxiliary/Auxiliary'
 import axios from '../../axios-order'
-const ToDoList = (props) =>{
-    const [input, setInput] = useState();
-    const [todo , setTodo] = useState('')
-    const getData = async () =>{
-        axios.get(`/data/${props.user.uid}.json`)
-          .then(function (response) {
-              const allTodo = response.data
-              setTodo(allTodo)
-            console.log(allTodo);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
-    useEffect(() => {
-        getData()
-    } , [])
-    const putInData = (content) => {
-        axios.post(`/data/${props.user.uid}.json` , {
-            content: content
-        })
-        .then(function (response) {
-            console.log(response);
-            setInput(' ')
-            getData()
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
-    const deleteData = (id , content) => {
-      axios.delete(`/data/${props.user.uid}/${id}.json`, {
-    data: {content}
+import classes from './ToDoList.module.css';
+const ToDoList = (props) => {
+  const [input, setInput] = useState();
+  const [todo, setTodo] = useState('')
+  const getData = async () => {
+    axios.get(`/data/${props.user.uid}.json`)
+      .then(function (response) {
+        const allTodo = response.data
+        setTodo(allTodo)
+        console.log(allTodo);
       })
-    .then(function (response) {
-      getData()
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  const putInData = (content) => {
+    axios.post(`/data/${props.user.uid}.json`, {
+      content: content
+    })
+      .then(function (response) {
+        console.log(response);
+        setInput(' ')
+        getData()
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  const deleteData = (id, content) => {
+    axios.delete(`/data/${props.user.uid}/${id}.json`, {
+      data: { content }
+    })
+      .then(function (response) {
+        getData()
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
-    }
-    let dataOut = ''
-    if(todo){
-      dataOut= Object.entries( todo )
-        .map( el => {
-          console.log(el)
-            return <li id={el[0]} key={el[0]}  onClick={(e) => deleteData(el[0] , el[1].content )}>{el[1].content}</li>
-        })
-    }
-    
+  }
+  let dataOut = ''
+  if (todo) {
+    dataOut = Object.entries(todo)
+      .map(el => {
+        console.log(el)
+        return <li id={el[0]} key={el[0]}>
+          {el[1].content}
+          <Button className={classes.button} variant="contained" onClick={(e) => deleteData(el[0], el[1].content)}>Delete</Button>
+          </li>
+      })
+  }
 
-      console.log(props.user.photoURL)
-    return(
+
+  console.log(props.user.photoURL)
+  return (
     <Aux>
 
-    <Typography variant="h3">To do list</Typography>
-    <Typography variant="h3">Welcome {props.user.displayName} <Avatar src={props.user.photoURL}></Avatar></Typography>
-    
+      <Typography variant="h3">To do list</Typography>
+      <Typography variant="h3">Welcome {props.user.displayName} <img className={classes.img} src={props.user.photoURL} alt={props.user.photoURL}></img></Typography>
+
       <div className="enterNew">
-      <TextField id="standard-basic" label="New to-do" value={input} onChange={e => setInput(e.target.value)} />
-      <Button variant="contained" onClick={() => putInData(input)}>Enter</Button>
-      <Button variant="contained">Delete</Button>
-      <ul>
-      {dataOut}
-      </ul>
+        <TextField id="standard-basic" label="New to-do" value={input} onChange={e => setInput(e.target.value)} />
+        <Button variant="contained" onClick={() => putInData(input)}>Enter</Button>
+
       </div>
+      <ul className={classes.main}>
+        {dataOut}
+      </ul>
     </Aux>
-    )
+  )
 }
 
 export default ToDoList
